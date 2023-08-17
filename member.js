eaa8dc117d39cccfb2b7ac18a10be299c95944e4
oyvindo <29068849+oyvindhiof@users.noreply.github.com>
2023-08-17 15:36:58 +0000

    second

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'];
+    var member = {
+        name: 'John',
+        age: 30,
+        skills: skills
+    };
+    return member;
+}
